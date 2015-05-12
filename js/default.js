e6923656bf3d4655a48ad1aef930225795e1e187
MatthewTeta <randommagicmatt@gmail.com>
2015-05-11 21:03:51 -0600

    Added all files

---
+++ js/default.js
@@ -0,0 +1,9 @@
+//function resize() {
+//	$('body').css('font-size', $(window).width() / ($(window).width() / 1920 * 33.33 + 66.66));
+//	$('section#header h1').css('padding-top', (($(window).height() - $('h1').height()) / 2) + 'px');
+//	$('section#header h3').css('width', 'calc(' + $('span').width() + 'px - 1em)');
+//}
+//
+//$(document).ready(resize);
+//
+//$(window).resize(resize);
---
+++ js/home.js
@@ -0,0 +1,91 @@
+
+$(document).ready(function() {
+	
+//	$('section#info').hide();
+//	
+//	$('body').on('click', function() {
+////		$('section#header').hide();
+//		$('section#info').show();
+//	});
+	
+	$('h3').addClass('done');
+	$('h3:first').removeClass('done');
+
+	setTimeout(function() {
+		$('h3:first').html('W');
+		setTimeout(function() {
+			$('h3:first').html('WF');
+			setTimeout(function() {
+				$('h3:first').html('WPF');
+				setTimeout(function() {
+					$('h3:first').html('WPF&nbsp;');
+					setTimeout(function() {
+						$('h3:first').html('WPF F');
+						setTimeout(function() {
+							$('h3:first').html('WPF FS');
+							setTimeout(function() {
+								$('h3:first').html('WPF FSA');
+								setTimeout(function() {
+									$('h3:first').html('WPF FSA_');
+									setTimeout(function() {
+										$('h3:first').html('WPF FSA_A');
+										setTimeout(function() {
+											$('h3:first').html('WPF FSA_AP');
+											setTimeout(function() {
+												$('h3:first').html('WPF FSA_AP&nbsp;');
+												setTimeout(function() {
+													$('h3:first').html('WPF FSA_AP G');
+													setTimeout(function() {
+														$('h3:first').html('WPF FSA_AP GE');
+														setTimeout(function() {
+															$('h3:first').html('WPF FSA_AP GET');
+															setTimeout(function() {
+																$('h3:first').addClass('done');
+																$('h3:nth-of-type(2)').removeClass('done');
+																setTimeout(function() {
+																	$('h3:nth-of-type(2)').addClass('done');
+																	$('h3:nth-of-type(2)').html('Locating Password Dir');
+																	setTimeout(function() {
+																		$('h3:nth-of-type(2)').html('Locating Password Dir.');
+																		setTimeout(function() {
+																			$('h3:nth-of-type(2)').html('Locating Password Dir..');
+																			setTimeout(function() {
+																				$('h3:nth-of-type(2)').html('Locating Password Dir...');
+setTimeout(function() {
+	$('h3:nth-of-type(3)').html('Success');
+	setTimeout(function() {
+		$('h3:nth-of-type(4)').html('Found Crypto Keys');
+		setTimeout(function() {
+			$('h3:nth-of-type(5)').html('Decrypting Password');
+			setTimeout(function() {
+				$('h3:nth-of-type(6)').html('Success');
+				setTimeout(function() {
+					$('h3:nth-of-type(7)').html('Password: Click Here To Buy');
+					$('h1 span').html('[Admin] Root>&nbsp;');
+					$('h3:nth-of-type(8)').removeClass('done');
+				}, 300);
+			}, 300);
+		}, 300);
+	}, 300);
+}, 300);
+																			}, 1500);
+																		}, 1500);
+																	}, 1500);
+																}, 1000);
+															}, 2500);
+														}, 375);
+													}, 375);
+												}, 375);
+											}, 1000);
+										}, 375);
+									}, 375);
+								}, 375);
+							}, 375);
+						}, 375);
+					}, 375);
+				}, 1000);
+			}, 375);
+		}, 375);
+	}, 2500);
+	
+});
